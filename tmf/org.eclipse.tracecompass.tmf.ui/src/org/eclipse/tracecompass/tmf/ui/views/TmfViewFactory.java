db4ffd90257daa75fbbaacc7e81d8af0e34f27d0
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2016-08-02 15:38:56 -0400

    Add tmfViewFactory
    Change-Id: I34b879af5ac0c893354e1ba919376aec067c0a4f

---
+++ tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/views/TmfViewFactory.java
@@ -0,0 +1,62 @@
+package org.eclipse.tracecompass.tmf.ui.views;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * @author Jonathan Rajotte-Julien
+ * @since 2.1
+ *
+ */
+public class TmfViewFactory {
+
+    private static TmfViewFactory fInstance = null;
+
+    private Map<String, TmfView> clonedViewData = new HashMap<>();
+
+    private TmfViewFactory() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @return The singleton instance
+     */
+    public static TmfViewFactory getInstance() {
+        if (fInstance == null) {
+            fInstance = new TmfViewFactory();
+        }
+
+        return fInstance;
+    }
+
+    /**
+     *
+     * @param view
+     *          The view to be  cloned
+     * @return
+     * @throws PartInitException
+     */
+    public void clone(TmfView view) throws PartInitException {
+
+        if (view == null) {
+            return;
+        }
+
+        String primaryId = view.getViewSite().getId();
+        String secondaryId = UUID.randomUUID().toString();
+
+        clonedViewData.put(primaryId + '.' + secondaryId , view);
+
+        IWorkbench workbench = PlatformUI.getWorkbench();
+        IWorkbenchWindow workbenchWindow = workbench.getActiveWorkbenchWindow();
+        IWorkbenchPage page = workbenchWindow.getActivePage();
+        page.showView(primaryId, secondaryId, IWorkbenchPage.VIEW_VISIBLE);
+    }
+}
