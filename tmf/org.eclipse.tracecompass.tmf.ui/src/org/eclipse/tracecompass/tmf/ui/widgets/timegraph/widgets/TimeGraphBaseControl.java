3725e119b7d1846cc5852646660fefd65dc585a8
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2016-10-27 15:21:14 -0400

    Add pin state to TimeGraphBaseControl
    Change-Id: I538606eb0ebcac832fcd1905c8adf0d70330577b
    Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>

--- tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/widgets/timegraph/widgets/TimeGraphBaseControl.java
+++ tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/widgets/timegraph/widgets/TimeGraphBaseControl.java
@@ -113,5 +113,15 @@ public abstract class TimeGraphBaseControl extends Canvas implements PaintListen
         return fFontHeight;
     }
 
+    /**
+     * Set the pin state
+     *
+     * @param pinned
+     *            The pin state
+     * @since 2.2
+     */
+    public synchronized void setPinned(boolean pinned) {
+    }
+
     abstract void paint(Rectangle bound, PaintEvent e);
 }
