80307b1c94046ddebe70cf8841c73161b30defab
Alexandre Montplaisir <alexmonthy@efficios.com>
2016-12-07 14:39:58 -0500

    wip-property-tester-testing
    Change-Id: I6b79e0805934a73856672b5f09b9091eb25d9d91

---
+++ ctf/org.eclipse.tracecompass.tmf.ctf.ui/src/org/eclipse/tracecompass/internal/tmf/ctf/ui/streamintersection/CtfTmfTracePropertyTester.java
@@ -0,0 +1,14 @@
+package org.eclipse.tracecompass.internal.tmf.ctf.ui.streamintersection;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.jdt.annotation.Nullable;
+
+public class CtfTmfTracePropertyTester extends PropertyTester {
+
+    @Override
+    public boolean test(@Nullable Object receiver, @Nullable String property, Object @Nullable [] args, @Nullable Object expectedValue) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
